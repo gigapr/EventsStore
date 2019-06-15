6598c6e3961638d9cfd40e1ba340cf0c0b007a32
Gaetano Santonastaso <gigapr@hotmail.com>
2019-06-15 18:02:21 +0100

    refactored eventsstore so it open a connection to the database once when initialised

--- src/eventStore.go
+++ src/eventStore.go
@@ -11,46 +11,41 @@ import (
 
 //EventsStore is responsible for storing and retrieving events
 type EventsStore struct {
-	log          *logrus.Logger
-	host         string
-	port         int
-	username     string
-	password     string
-	databaseName string
+	log *logrus.Logger
+	db  *sql.DB
 }
 
 //NewEventsStore creates an instance of the EventsStore
 func NewEventsStore(host string, port int, username string, password string, databaseName string) *EventsStore {
 
-	return &EventsStore{
-		host:         host,
-		port:         port,
-		username:     username,
-		password:     password,
-		databaseName: databaseName,
-		log:          logrus.New(),
-	}
-}
+	l := logrus.New()
 
-//Save stores an event to the database
-func (es EventsStore) Save(sourceID string, EventID string, eventType string, data []byte, metadata []byte) (int, error) {
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		es.host, es.port, es.username, es.password, es.databaseName)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, username, password, databaseName)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		es.log.WithFields(logrus.Fields{
-			"message": "Unable to open connection to database.",
-		}).Error(err)
+		l.WithFields(logrus.Fields{"message": "Unable to open connection to database."}).Fatal(err)
 	}
-	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		l.WithFields(logrus.Fields{"message": "Failed to execute ping against database."}).Fatal(err)
+	}
+
+	return &EventsStore{
+		log: l,
+		db:  db,
+	}
+}
+
+//Save an event to the database
+func (es EventsStore) Save(sourceID string, EventID string, eventType string, data []byte, metadata []byte) (int, error) {
 
 	sqlStatement := `INSERT INTO Events (SourceId, EventId, EventType, EventData, Metadata)
 					 VALUES 			($1, 	   $2, 		$3, 	   $4, 		  $5)
 					 RETURNING id`
 	id := 0
-	err = db.QueryRow(sqlStatement, sourceID, EventID, eventType, data, metadata).Scan(&id)
+	err := es.db.QueryRow(sqlStatement, sourceID, EventID, eventType, data, metadata).Scan(&id)
 	if err != nil {
 		return -1, err
 	}
@@ -58,24 +53,14 @@ func (es EventsStore) Save(sourceID string, EventID string, eventType string, da
 	return id, nil
 }
 
+// Exists checks if an event already exists in the store
 func (es EventsStore) Exists(sourceID string, EventID string) (bool, error) {
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		es.host, es.port, es.username, es.password, es.databaseName)
-
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		es.log.WithFields(logrus.Fields{
-			"message": "Unable to open connection to database.",
-		}).Error(err)
-	}
-	defer db.Close()
 
 	sqlStatement := `SELECT * FROM Events 
 					 WHERE SourceId = $1 AND EventId = $2
 					 fetch first 1 rows only`
 
-	rows, err := db.Query(sqlStatement, sourceID, EventID)
+	rows, err := es.db.Query(sqlStatement, sourceID, EventID)
 	defer rows.Close()
 
 	if err != nil {
