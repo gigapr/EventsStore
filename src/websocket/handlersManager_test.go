4f46f3ac5b2ed10b7dafc4c7def0284ee57ef0c2
Gaetano Santonastaso <gigapr@hotmail.com>
2019-07-08 23:18:51 +0100

    fixed docker file

--- src/websocket/handlersManager_test.go
+++ src/websocket/handlersManager_test.go
@@ -1,9 +1,13 @@
 package websocket //import "gigapr/eventsstore/websocket"
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_can_subscribe_to_a_topic(t *testing.T) {
-	handlersManager := NewHandlersManager()
+
+	handlersManager := new(HandlersManager)
+	handlersManager.Handlers = make(map[string][]chan []byte)
 
 	topic := "SomeTopic"
 	channel := handlersManager.Subscribe(topic)
@@ -12,51 +16,54 @@ func Test_can_subscribe_to_a_topic(t *testing.T) {
 		t.Error("HandlersManager should create channel")
 	}
 
-	handlers := len(handlersManager.handlers)
+	handlers := len(handlersManager.Handlers)
 	if handlers != 1 {
 		t.Errorf("HandlersManager should have 1 handlers, got: %d.", handlers)
 	}
 
-	channels := len(handlersManager.handlers[topic])
+	channels := len(handlersManager.Handlers[topic])
 	if channels != 1 {
 		t.Errorf("HandlersManager should have 1 channel for '%s', got: %d.", topic, channels)
 	}
 }
 
 func Test_when_subscribing_to_a_topic_should_handle_multiple_channels_on_the_same_topic(t *testing.T) {
-	handlersManager := NewHandlersManager()
+	handlersManager := new(HandlersManager)
+	handlersManager.Handlers = make(map[string][]chan []byte)
 
 	topic := "SomeTopic"
 	handlersManager.Subscribe(topic)
 	handlersManager.Subscribe(topic)
 
-	handlers := len(handlersManager.handlers)
+	handlers := len(handlersManager.Handlers)
 	if handlers != 1 {
 		t.Errorf("HandlersManager should have 1 channel, got: %d.", handlers)
 	}
 
-	channels := len(handlersManager.handlers[topic])
+	channels := len(handlersManager.Handlers[topic])
 	if channels != 2 {
 		t.Errorf("HandlersManager should have 2 channels for '%s', got: %d.", topic, channels)
 	}
 }
 
 func Test_can_unsubscribe_channel_from_a_topic(t *testing.T) {
-	handlersManager := NewHandlersManager()
+	handlersManager := new(HandlersManager)
+	handlersManager.Handlers = make(map[string][]chan []byte)
 
 	topic := "SomeTopic"
 	channel := handlersManager.Subscribe(topic)
 
 	handlersManager.Unsubscribe(topic, channel)
 
-	handlers := len(handlersManager.handlers)
+	handlers := len(handlersManager.Handlers)
 	if handlers != 0 {
 		t.Errorf("HandlersManager should have 0 channel, got: %d.", handlers)
 	}
 }
 
 func Test_when_unsubscribing_a_channel_doesnt_remove_handlers_if_there_are_subscribed_channels(t *testing.T) {
-	handlersManager := NewHandlersManager()
+	handlersManager := new(HandlersManager)
+	handlersManager.Handlers = make(map[string][]chan []byte)
 
 	topic := "SomeTopic"
 	handlersManager.Subscribe(topic)
@@ -64,12 +71,12 @@ func Test_when_unsubscribing_a_channel_doesnt_remove_handlers_if_there_are_subsc
 
 	handlersManager.Unsubscribe(topic, channel)
 
-	handlers := len(handlersManager.handlers)
+	handlers := len(handlersManager.Handlers)
 	if handlers != 1 {
 		t.Errorf("HandlersManager should have 1 channel, got: %d.", handlers)
 	}
 
-	channels := handlersManager.handlers[topic]
+	channels := handlersManager.Handlers[topic]
 	channelsLengths := len(channels)
 
 	if channelsLengths != 1 {
@@ -82,7 +89,8 @@ func Test_when_unsubscribing_a_channel_doesnt_remove_handlers_if_there_are_subsc
 }
 
 func Test_can_get_channels_for_a_topic(t *testing.T) {
-	handlersManager := NewHandlersManager()
+	handlersManager := new(HandlersManager)
+	handlersManager.Handlers = make(map[string][]chan []byte)
 
 	topic := "SomeTopic"
 	handlersManager.Subscribe(topic)
@@ -98,7 +106,8 @@ func Test_can_get_channels_for_a_topic(t *testing.T) {
 }
 
 func Test_retunrs_nil_if_there_areent_any_channels_for_a_topic(t *testing.T) {
-	handlersManager := NewHandlersManager()
+	handlersManager := new(HandlersManager)
+	handlersManager.Handlers = make(map[string][]chan []byte)
 
 	topic := "SomeTopic"
 
